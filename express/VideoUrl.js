c522a317cbce239b740fb17c38e6476cd1809c24
relsoul <admin@emufan.com>
2016-08-31 23:10:51 +0800

    update url match

--- express/VideoUrl.js
+++ express/VideoUrl.js
@@ -6,7 +6,7 @@
 let getVideoUrl = require("../list/getVideoUrl");
 
 module.exports = function (req, res) {
-    var url=req.params[0].replace(/http:\/\/m.dilidili.com\/|http:\/\/dilidili.com\//gi,"");
+    var url=req.params[0].replace(/http:\/\/m.dilidili.com\/|http:\/\/dilidili.com\/|http:\/\/www.dilidili.com\//gi,"");
 
     let c = "http://m.dilidili.com/" + url;
     console.log("url", c);
